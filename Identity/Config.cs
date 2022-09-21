c316c84c9e95606df44fcbf66c26c41b4ab7865c
Magda Warpas <magda.warpas@avanade.com>
2022-09-21 20:18:48 +0200

    initial setup

---
+++ Identity/Config.cs
@@ -0,0 +1,61 @@
+﻿using IdentityServer4.Models;
+
+namespace Identity
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> { "role" }
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new[] { new ApiScope("TutorAPI.read"), new ApiScope("TutorAPI.write") };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new[]
+            { 
+                new ApiResource("TutorAPI")
+                {
+                    Scopes = new List<string> { "TutorAPI.read", "TutorAPI.write" },
+                    ApiSecrets = new List<Secret> { new Secret("ScopeSecret".Sha256())},
+                    UserClaims = new List<string> { "role" }
+                }
+            };
+        public static IEnumerable<Client> Clients =>
+            new[]
+            {
+                // m2m
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("ClientSecret1".Sha256())},
+                    AllowedScopes = { "TutorAPI.read", "TutorAPI.write" }
+                },
+                // code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("ClientSecret1".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:7225/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:7225/sidnout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:7225/signout-callback-oidc" },
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "TutorAPI.read", "TutorAPI.write" },
+                    RequirePkce = true,
+                    RequireConsent = true,
+                    AllowPlainTextPkce = false
+                }
+            };
+    }
+}
---
+++ Identity/Program.cs
@@ -0,0 +1,71 @@
+using Identity;
+using Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var seed = args.Contains("/seed");
+
+if (seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+}
+
+var builder = WebApplication.CreateBuilder(args);
+
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+var assembly = typeof(Program).Assembly.GetName().Name;
+
+if (seed)
+{
+    SeedData.EnsureSeedData(defaultConnString);
+}
+
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
+// Add services to the container.
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseIdentityServer();
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+app.Run();
